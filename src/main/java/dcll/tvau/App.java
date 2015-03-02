48fd16eca7c0ead59ccc3129c3663504598d8276
Tetote <tetote@free.fr>
2015-03-02 01:52:22 +0100

    Minimal main using SimpleStack

--- src/main/java/dcll/tvau/App.java
+++ src/main/java/dcll/tvau/App.java
@@ -19,6 +19,24 @@ public final class App {
      *
      */
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        SimpleStack stack = new SimpleStackImpl();
+
+        String lawl = "lawl";
+
+        System.out.println("I store string '" + lawl + "' in stack");
+        stack.push(new Item(lawl));
+
+        System.out.println("When I peek from stack, "
+                + "it give '" + stack.peek().getObject() + "'");
+
+
+
+        String answer = "the answer to life, the universe and everything";
+
+        System.out.println("\nAnd I store string '" + answer + "' in stack");
+        stack.push(new Item(answer));
+
+        System.out.println("When I peek from stack, "
+                + "it give '42'");
     }
 }
